db70767dd0d4130929991a3dc94417fca058b96c
DivyaSobale <93645722+DivyaSobale@users.noreply.github.com>
2021-11-12 21:40:27 +0530

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime
+    hr.style.transform = `rotateZ(${hrotation} deg)`;
+    mn.style.transform = `rotateZ(${mrotation}deg)`;
+    sc.style.transform = `rotateZ(${srotation}deg)`;
+
+}, 1000);
